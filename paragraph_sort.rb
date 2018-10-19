17a34886975d5eaf8c0d5d0893d57e4335f9db3f
tchoutdara <monkeymilitia444@gmail.com>
2018-10-19 10:32:45 -0600

    final

--- paragraph_sort.rb
+++ paragraph_sort.rb
@@ -1,17 +1,60 @@
-@words = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac nisl at diam sagittis ferm
-entum. Nam varius mi augue, eget congue magna mattis dictum. Maecenas laoreet nisl gravida fermen
-tum pellentesque. Quisque finibus at ante nec volutpat. Phasellus tincidunt justo justo, vel port
-a ante interdum quis. In non arcu et lorem egestas maximus sit amet nec sapien. Proin quis pellen
-tesque nulla. Suspendisse ut nisl tellus. Morbi quis neque dolor. Donec commodo quis ante sed tin
-cidunt. Etiam convallis pulvinar egestas. Donec consequat libero magna, sit amet accumsan nisl gr
-avida non. Cras rutrum, metus quis posuere bibendum, risus tortor aliquet tortor, in venenatis ma
-ssa ex non magna. Integer porta tristique arcu vel congue. Donec eu gravida ipsum, quis sollicitu
-din urn"
+def sort
+    words = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac nisl at diam sagittis ferm
+    entum. Nam varius mi augue, eget congue magna mattis dictum. Maecenas laoreet nisl gravida fermen
+    tum pellentesque. Quisque finibus at ante nec volutpat. Phasellus tincidunt justo justo, vel port
+    a ante interdum quis. In non arcu et lorem egestas maximus sit amet nec sapien. Proin quis pellen
+    tesque nulla. Suspendisse ut nisl tellus. Morbi quis neque dolor. Donec commodo quis ante sed tin
+    cidunt. Etiam convallis pulvinar egestas. Donec consequat libero magna, sit amet accumsan nisl gr
+    avida non. Cras rutrum, metus quis posuere bibendum, risus tortor aliquet tortor, in venenatis ma
+    ssa ex non magna. Integer porta tristique arcu vel congue. Donec eu gravida ipsum, quis sollicitu
+    din urn"
 
-def word 
-    result = Hash.new(0)
-    @words.each { |word| result[word] += 1 }
-    puts result
-end
+    words = words.downcase.gsub(/[^a-z0-9\s]/i, '')
+    arr = words.split(' ')
+    hash = Hash.new(0)
+    arr.each do |word|
+        hash[word] += 1
+    end
+    puts "Would you like asc or desc?"
+    input = gets.strip
+    if input == 'asc'
+      puts hash.sort { |lorem,ipsum| lorem[1]<=>ipsum[1] }.to_h
+    elsif input == 'desc'
+      puts hash.sort { |lorem,ipsum| lorem[1]<=>ipsum[1] }.reverse.to_h
+    else
+      menu
+    end
+  end
+  
+sort
 
-word()
+# def menu
+#     my_str='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac nisl at diam sagittis ferm
+#     entum. Nam varius mi augue, eget congue magna mattis dictum. Maecenas laoreet nisl gravida fermen
+#     tum pellentesque. Quisque finibus at ante nec volutpat. Phasellus tincidunt justo justo, vel port
+#     a ante interdum quis. In non arcu et lorem egestas maximus sit amet nec sapien. Proin quis pellen
+#     tesque nulla. Suspendisse ut nisl tellus. Morbi quis neque dolor. Donec commodo quis ante sed tin
+#     cidunt. Etiam convallis pulvinar egestas. Donec consequat libero magna, sit amet accumsan nisl gr
+#     avida non. Cras rutrum, metus quis posuere bibendum, risus tortor aliquet tortor, in venenatis ma
+#     ssa ex non magna. Integer porta tristique arcu vel congue. Donec eu gravida ipsum, quis sollicitu
+#     din urna.'
+  
+#     my_str = my_str.downcase.gsub(/[^a-z0-9\s]/i, '')
+#     arr = my_str.split(" ")
+#     my_hash = Hash.new(0)
+#     arr.each do |w|
+#       my_hash[w] += 1
+#     end
+  
+#     puts "Would you like asc or desc?"
+#     input = gets.strip
+#     if input == 'asc'
+#       puts my_hash.sort { |lorem,ipsum| lorem[1]<=>ipsum[1] }.to_h
+#     elsif input == 'desc'
+#       puts my_hash.sort { |lorem,ipsum| lorem[1]<=>ipsum[1] }.reverse.to_h
+#     else
+#       menu
+#     end
+#   end
+  
+#   menu
